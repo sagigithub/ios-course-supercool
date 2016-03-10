c89f84a210d54b8158c7cdceebae3307633f7de4
Sagi Herman <sagiherman@Sagi-Hermans-MacBook-Pro-2.local>
2016-03-10 15:57:52 +0200

    first save

--- firstapp/ViewController.swift
+++ firstapp/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coollogo: UIImageView!
+    @IBOutlet weak var coolbg: UIImageView!
+    @IBOutlet weak var uncoolbutton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makemenotsouncool(sender: AnyObject) {
+        coolbg.hidden = false
+        coollogo.hidden=false
+        uncoolbutton.hidden=true
+    }
 
 }
 
